51988661086ec6d4cc66f6320b9436bafe84e63e
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2025-01-05 02:15:55 +0530

    connected database and created user model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
-app.listen(5001, () => {
-  console.log("server is running on port 5001");
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+  connectDB();
 });

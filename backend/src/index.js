996733d8928c425f792f4a5a78d9cd93d8225e6c
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2025-01-05 12:45:13 +0530

    signup completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
